857136a7cef9925f85ed72a24303056239e99f86
acostajosue880 <acostajosue880@gmail.com>
2024-08-10 21:59:28 +0000

    cambios

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,29 @@
 import 'package:flutter/material.dart';
 //import 'package:cloud_firestore/cloud_firestore.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Votaciones Albumes de Rock!'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Ingresa tu votacion:'),
-            
+          appBar: AppBar(
+            title: const Text('Votaciones Albumes de Rock!'),
+            backgroundColor: Colors.red,
+            titleTextStyle: const TextStyle(fontSize: 20),
           ),
-        ),
-      ),
+          body: Center(
+            child: TextButton(
+              onPressed: () {}, 
+              child: const Text('Ingresa tu votacion:'),
+              
+            ),
+          )),
     );
   }
 }
