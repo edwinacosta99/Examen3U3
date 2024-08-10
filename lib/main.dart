ec66a71688a884276cd6de5a5efd30f87304a4fd
Edwin Acosta <edwin.acosta@unah.hn>
2024-08-10 16:15:38 -0600

    cambios

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,26 @@
 import 'package:flutter/material.dart';
-//import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'add_album_screen.dart';
+import 'album_list_screen.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Votaciones Albumes de Rock!'),
-            backgroundColor: Colors.red,
-            titleTextStyle: const TextStyle(fontSize: 20),
-          ),
-          body: Center(
-            child: TextButton(
-              onPressed: () {}, 
-              child: const Text('Ingresa tu votacion:'),
-            ),
-          )),
+      title: 'Rock Album Voting App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: AlbumListScreen(),
+      routes: {
+        '/addAlbum': (context) => AddAlbumScreen(),
+      },
     );
   }
 }
